b2c8cb4e5bc90c371ce2b5af6a988c0286669162
Nelocho <nelson.a.nunez.m@gmail.com>
2017-12-19 22:40:49 -0400

    Inicio de proyecto
    Calculadora Numerologa de pitagoras y caldeo

---
+++ CalculadoraNumerologa/src/calculadoranumerologa/CalculadoraNumerologa.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoranumerologa;
+
+/**
+ *
+ * @author Nelson
+ */
+public class CalculadoraNumerologa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String aux="Nelson";
+        FuncionesCalculadora a=new FuncionesCalculadora();
+        a.recepcionNombre(aux, 3);
+        a.recepcionNombre(aux, 2);
+  
+       
+       // System.out.println(aux.charAt(3));
+    }
+    
+}
